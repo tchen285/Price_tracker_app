07154f9cc2726045a18862634794f3a190c6932e
tchen285 <91436200+tchen285@users.noreply.github.com>
2024-02-25 17:41:15 -0800

    fixed the bug that cannot run flask run

--- .venv/bin/activate.fish
+++ .venv/bin/activate.fish
@@ -33,7 +33,7 @@ end
 # Unset irrelevant variables.
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV "/Users/taochen/Desktop/Python Code/Price_tracker/Price_tracker_app/.venv"
+set -gx VIRTUAL_ENV "/Users/taochen/Desktop/PythonCode/Price_tracker/Price_tracker_app/.venv"
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/bin" $PATH
