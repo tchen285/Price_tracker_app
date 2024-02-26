8e772ddf6adc90dd1d50a2d570a9fd36e44d4e46
tchen285 <91436200+tchen285@users.noreply.github.com>
2024-02-26 14:09:43 -0800

    wrote in .csv works

--- main.py
+++ main.py
@@ -1,28 +1,48 @@
 from bestbuy import BestBuyScraper
-from flask import Flask, render_template, session
+from flask import Flask, render_template, session, request, redirect, url_for
+import csv
 
 app = Flask(__name__)
 app.secret_key = '123890'  # Set a secret key for session
 
 bestbuy_scraper = BestBuyScraper()
-bestbuy_price = bestbuy_scraper.get_bestbuy_price("https://www.bestbuy.com/site/sony-playstation-5-slim-console-white/6566039.p?skuId=6566039")
+bestbuy_price = bestbuy_scraper.get_bestbuy_price("https://www.bestbuy.com/site/alienware-aw3423dwf-34-quantum-dot-oled-curved-ultrawide-gaming-monitor-165hz-amd-freesync-premium-pro-vesa-hdmiusb-dark-side-of-the-moon/6536990.p?skuId=6536990")
+
 
 @app.route("/")
 def select_url():
     session['bestbuy_price'] = bestbuy_price
     return render_template("index.html", price=bestbuy_price)
 
-@app.route("/left-sidebar.html")
+
+@app.route('/left-sidebar.html', methods=['GET', 'POST'])
 def left_sidebar():
-    return render_template("left-sidebar.html")
+    if request.method == 'POST':
+        # Retrieve form data
+        url = request.form.get('urlInput')
+        target_price = request.form.get('targetPrice')
+        email = request.form.get('email')
+        print(url)
+        print(target_price)
+        print(email)
+        # Process the form data (e.g., write to CSV file)
+        with open('data.csv', 'a', newline='') as csvfile:
+            csv_writer = csv.writer(csvfile)
+            csv_writer.writerow([url, target_price, email])
+
+    # Render the template with the form
+    return render_template('left-sidebar.html')
+
 
 @app.route("/right-sidebar.html")
 def right_sidebar():
     return render_template("right-sidebar.html")
 
+
 @app.route("/no-sidebar.html")
 def no_sidebar():
     return render_template("no-sidebar.html")
+
 
 if __name__ == "__main__":
     app.run(debug=True)
