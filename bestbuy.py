8d3436552bdf40b8e2fe95b0839ea1f1597d9c76
tchen285 <91436200+tchen285@users.noreply.github.com>
2024-02-25 16:52:57 -0800

    changed a file name

---
+++ bestbuy.py
@@ -0,0 +1,34 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+
+
+class BestBuyScraper:
+    def get_bestbuy_price(self, product_url):
+        # Keep Chrome browser open after the program finishes
+        chrome_options = webdriver.ChromeOptions()
+        chrome_options.add_experimental_option("detach", True)
+
+        driver = webdriver.Chrome(options=chrome_options)
+
+        # 3. Visit the website
+        # driver.get(
+        #     "https://www.bestbuy.com/site/microsoft-xbox-series-s-1tb-all-digital-console-disc-free-gaming-black/6547877.p?skuId=6547877")
+        driver.get(product_url)
+
+        # 4. Find the price element using its class name
+        price_element = driver.find_element(By.CLASS_NAME, "priceView-hero-price.priceView-customer-price")
+
+        # 5. Find the inner span with the actual price
+        price_span = price_element.find_element(By.CSS_SELECTOR, 'span[aria-hidden="true"]')
+
+        # 6. Get the text content of the span
+        price_str = price_span.text
+
+        price_float = float(price_str.replace('$', ''))
+
+        # 8. Close the browser
+        driver.quit()
+
+        return price_float
+
+
