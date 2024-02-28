340eecad51c03ef2b07a0d2e427669f22f1927c6
tchen285 <91436200+tchen285@users.noreply.github.com>
2024-02-27 19:03:38 -0800

    Can show multiple price on main page

--- main.py
+++ main.py
@@ -6,13 +6,14 @@ app = Flask(__name__)
 app.secret_key = '123890'  # Set a secret key for session
 
 bestbuy_scraper = BestBuyScraper()
-bestbuy_price = bestbuy_scraper.get_bestbuy_price("https://www.bestbuy.com/site/alienware-aw3423dwf-34-quantum-dot-oled-curved-ultrawide-gaming-monitor-165hz-amd-freesync-premium-pro-vesa-hdmiusb-dark-side-of-the-moon/6536990.p?skuId=6536990")
+bestbuy_price1 = bestbuy_scraper.get_bestbuy_price("https://www.bestbuy.com/site/alienware-aw3423dwf-34-quantum-dot-oled-curved-ultrawide-gaming-monitor-165hz-amd-freesync-premium-pro-vesa-hdmiusb-dark-side-of-the-moon/6536990.p?skuId=6536990")
+bestbuy_price2 = bestbuy_scraper.get_bestbuy_price("https://www.bestbuy.com/site/bose-ultra-open-ear-true-wireless-open-earbuds-black/6568949.p?skuId=6568949")
 
 
 @app.route("/")
 def select_url():
-    session['bestbuy_price'] = bestbuy_price
-    return render_template("index.html", price=bestbuy_price)
+    session['bestbuy_price1'] = bestbuy_price1
+    return render_template("index.html", price1=bestbuy_price1, price2=bestbuy_price2)
 
 
 @app.route('/left-sidebar.html', methods=['GET', 'POST'])
@@ -23,12 +24,13 @@ def left_sidebar():
         target_price = request.form.get('targetPrice')
         email = request.form.get('email')
 
-        # Process the form data (e.g., write to CSV file)
-        with open('data.csv', 'a', newline='') as csvfile:
-            csv_writer = csv.writer(csvfile)
-            csv_writer.writerow([url, target_price, email])
+        while url and target_price and email:
+            # Process the form data (e.g., write to CSV file)
+            with open('data.csv', 'a', newline='') as csvfile:
+                csv_writer = csv.writer(csvfile)
+                csv_writer.writerow([url, target_price, email])
 
-        return redirect(url_for('no_sidebar'))
+            return redirect(url_for('no_sidebar'))
 
     # Render the template with the form
     return render_template('left-sidebar.html')
