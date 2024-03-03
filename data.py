275e5e9448e05e5beff7893bdc59ac50f8beed75
tchen285 <91436200+tchen285@users.noreply.github.com>
2024-03-03 11:57:31 -0800

    data.py works

--- data.py
+++ data.py
@@ -1,5 +1,12 @@
-import pandas
+import pandas as pd
 
+# Read the CSV file
+data = pd.read_csv("/Users/taochen/Desktop/PythonCode/Price_tracker/Price_tracker_app/data.csv")
 
-class DataScraper:
-    def checkData(self):
+# Check the column names and remove leading/trailing whitespaces
+data.columns = data.columns.str.strip()
+
+# Iterate over rows and print email values
+for row in data.itertuples(index=False):
+    email_value = row.email
+    print(f"Email: {email_value}")
