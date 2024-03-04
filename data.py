eadc4a9bfb129fcb184ccb3b3f075eb8505e2ff8
tchen285 <91436200+tchen285@users.noreply.github.com>
2024-03-03 16:56:23 -0800

    it's able to send the email

--- data.py
+++ data.py
@@ -1,4 +1,8 @@
 import pandas as pd
+import smtplib
+
+MY_EMAIL = "tony.chent6@gmail.com"
+MY_PASSWORD = "ijfd bkrh ekji psqe"
 
 # Read the CSV file
 data = pd.read_csv("/Users/taochen/Desktop/PythonCode/Price_tracker/Price_tracker_app/data.csv")
@@ -10,3 +14,12 @@ data.columns = data.columns.str.strip()
 for row in data.itertuples(index=False):
     email_value = row.email
     print(f"Email: {email_value}")
+
+with smtplib.SMTP("smtp.gmail.com") as connection:
+    connection.starttls()
+    connection.login(MY_EMAIL, MY_PASSWORD)
+    connection.sendmail(
+        from_addr=MY_EMAIL,
+        to_addrs="tchen285@ucr.edu",
+        msg="Subject:From Price Tracker: It's time to place your order!\n\nHi There"
+    )
